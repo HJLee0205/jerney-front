ac8352b685be93f7aec2ac8efdb1688a00626084
HuijaeLee <Huijae.Lee@cndfactory.com>
2023-02-27 18:29:25 +0900

    axios 임포트

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
+import React from "react";
+import axios from "axios";
 import {BrowserView, MobileView} from "react-device-detect";
 import FunctionComponent from "./functionComponent";
 import ClassComponent from "./classComponent";
 import MainPage from "./components/mainPage";
 
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserView>
-          {/*<h1> React Practice </h1>*/}
-            {/*<h2> 함수형 컴포넌트 </h2>*/}
-            {/*    <FunctionComponent />*/}
-            {/*<h2> 클래스형 컴포넌트 </h2>*/}
-            {/*    <ClassComponent />*/}
-            <h2> Main </h2>
-                <MainPage />
-      </BrowserView>
-      <MobileView>
-          Mobile Page
-      </MobileView>
-    </div>
-  );
+class App extends React.Component {
+    state = {
+        isLoading: true
+    };
+
+    // 마운트 된 후 실행되는 함수
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({ isLoading: false })
+        }, 5000);
+    }
+
+    render() {
+        return <div>{this.state.isLoading ? "Loading..." : "Complete"}</div>
+    }
 }
 
 
